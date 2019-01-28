f48bd5dd62d00fe571cd52e7aeabe43db8a12b5a
C. Dylan Shearer <dylan@nekonya.info>
2019-01-27 20:18:09 -0800

    lock: Use RGB LED to show status

--- src/lock/status_light.cpp
+++ src/lock/status_light.cpp
@@ -1,13 +1,38 @@
 #include "status_light.h"
 #include <Arduino.h>
 
-#define YELLOW_PIN  3
-#define RED_PIN     2
+#define LED_RED_PIN     9
+#define LED_GREEN_PIN   6
+#define LED_BLUE_PIN    5
+
+#define LED_HIGH    64
+ 
+struct color_t
+{
+    int red;
+    int green;
+    int blue;
+
+    color_t(int r, int g, int b)
+    : red(r), green(g), blue(b) {}
+};
+
+static const color_t g_engaged_color(LED_HIGH, 0, 0);
+static const color_t g_disengaged_color(0, LED_HIGH, 0);
+
+static void showColor(const color_t color)
+{
+    analogWrite(LED_RED_PIN,    color.red);
+    analogWrite(LED_GREEN_PIN,  color.green);
+    analogWrite(LED_BLUE_PIN,   color.blue);
+}
 
 void status_light::setup()
 {
-    pinMode(YELLOW_PIN, OUTPUT);
-    pinMode(RED_PIN,    OUTPUT);
+    // init pins
+    pinMode(LED_RED_PIN,    OUTPUT);
+    pinMode(LED_GREEN_PIN,  OUTPUT);
+    pinMode(LED_BLUE_PIN,   OUTPUT);
 }
 
 void status_light::show_status(status_t status)
@@ -17,14 +42,12 @@ void status_light::show_status(status_t status)
 
     // show light
     switch (status) {
-    case STATUS_DISENGAGED:
-        digitalWrite(YELLOW_PIN,    HIGH);
-        digitalWrite(RED_PIN,       LOW);
+    case STATUS_ENGAGED:
+        showColor(g_engaged_color);
         break;
 
     default:
-        digitalWrite(YELLOW_PIN,    LOW);
-        digitalWrite(RED_PIN,       HIGH);
+        showColor(g_disengaged_color);
         break;
     }
 }
