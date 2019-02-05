6ef30feb6ebf1622c32673114d7fc45b0fb7ce0b
C. Dylan Shearer <dylan@nekonya.info>
2019-02-04 19:57:28 -0800

    remote: Sleep CPU when idle

--- src/remote/main.cpp
+++ src/remote/main.cpp
@@ -4,6 +4,7 @@ Remote main.
 
 #include <Arduino.h>
 #include <radio.h>
+#include <LowPower.h>
 #include "status_light.h"
 #include "control.h"
 
@@ -19,5 +20,10 @@ void setup()
 
 void loop()
 {
+  // LowPower.idle(SLEEP_FOREVER, ADC_ON, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_ON, USART0_ON, TWI_ON);
+  Serial.print("Loop\n");
   control::loop();
+
+  Serial.flush();
+  LowPower.powerSave(SLEEP_FOREVER, ADC_OFF, BOD_OFF, TIMER2_OFF);
 }
